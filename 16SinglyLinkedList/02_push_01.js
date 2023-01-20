c36f037c897d70adc8828dfa757658a3815d3d53
divinity6 <divinity666@naver.com>
2023-01-20 23:04:41 +0900

    [ feature ][ SinglyLinkedList > 2 ]push_메서드_작성

---
+++ 16SinglyLinkedList/02_push_01.js
@@ -0,0 +1,80 @@
+console.log( '=========================== push =============================' );
+
+/**
+ *  - linked list 는 단지 노드들의 집합이라는 것을 기억해야 한다
+ *
+ *  @pseudocode
+ *
+ *  - 주어진 값을 받아들인 후 그것을 이용해 새로운 Node 를 생성한다
+ *
+ *  - 만약, head 가 없다면 해당 list 가 비어있다는 것을 의미한다
+ *
+ *  - 따라서, head 와 tail 모두 새로운 Node 를 가르키게 하면 된다
+ *
+ *  - 만약, list 가 비어있지 않다면 마지막 Node 의 next 를 새롭게 생성된 Node 를 가르키게 하고,
+ *    tail 이 새롭게 생성된 Node 를 가르키도록 설정하면된다
+ *
+ *    ( 답지를 다알려주네 ㅋㅋㅋ )
+ *
+ *  - 마지막으로 length 를 1 증가시키면 된다
+ */
+
+/**
+ *
+ * @property { any } val - 해당 데이터
+ * @property { any } next - 다음 노드의 참조 정보
+ */
+class Node {
+
+    constructor( val ) {
+
+        this.val = val;
+
+        this.next = null;
+
+    }
+}
+/** mySolution */
+class SinglyLinkedList {
+
+    constructor() {
+
+        this.head = null;
+
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    push( val ){
+
+        const node = new Node( val )
+
+        /** head 가 존재하지 않으면 초기값에 추가 */
+        if ( !this.head ){
+            this.head = node;
+        }
+        /** 기존 마지막 값의 다음을 들어온 Node 로 설정 */
+        else {
+            this.tail.next = node;
+        }
+        /** 마지막 값을 해당 Node 로 설정 */
+        this.tail = node;
+
+        this.length += 1;
+
+    }
+
+}
+
+var list = new SinglyLinkedList();
+list.push( "HELLO" );
+list.push( "GOODBYE" );
+list.push( "NOO!!!" );
+
+console.log( "list" , list )
+
+/**
+ * - 진수를 이용한 방법
+ */
+/** goodSolution */
