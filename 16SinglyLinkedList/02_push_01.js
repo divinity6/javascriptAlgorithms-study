46dca4d56ff33a3b2599e121ff19e30b30f89411
divinity6 <divinity666@naver.com>
2023-01-23 17:51:52 +0900

    [ refactor ][ SinglyLinkedList > 2 ]push_메서드_구현

--- 16SinglyLinkedList/02_push_01.js
+++ 16SinglyLinkedList/02_push_01.js
@@ -11,7 +11,7 @@ console.log( '=========================== push =============================' );
  *
  *  - 따라서, head 와 tail 모두 새로운 Node 를 가르키게 하면 된다
  *
- *  - 만약, list 가 비어있지 않다면 마지막 Node 의 next 를 새롭게 생성된 Node 를 가르키게 하고,
+ *  - 만약, list 가 비어있지 않다면 기존의 마지막 Node 의 next 를 새롭게 생성된 Node 를 가르키게 하고,
  *    tail 이 새롭게 생성된 Node 를 가르키도록 설정하면된다
  *
  *    ( 답지를 다알려주네 ㅋㅋㅋ )
@@ -56,13 +56,52 @@ class SinglyLinkedList {
         }
         /** 기존 마지막 값의 다음을 들어온 Node 로 설정 */
         else {
+            /** 여기의 tail 은 기존 tail */
             this.tail.next = node;
         }
+
         /** 마지막 값을 해당 Node 로 설정 */
         this.tail = node;
 
         this.length += 1;
 
+    }
+
+}
+
+/**
+ * - 내꺼 코드랑 로직이똑같네 ㅋㅋ
+ *
+ * --> 거기서 거기...
+ */
+/** goodSolution */
+class SinglyLinkedList {
+
+    constructor() {
+
+        this.head = null;
+
+        this.tail = null;
+
+        this.length = 0;
+    }
+
+    push( val ){
+
+        const newNode = new Node( val )
+
+        if ( !this.head ){
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        return this;
     }
 
 }
@@ -73,8 +112,3 @@ list.push( "GOODBYE" );
 list.push( "NOO!!!" );
 
 console.log( "list" , list )
-
-/**
- * - 진수를 이용한 방법
- */
-/** goodSolution */
